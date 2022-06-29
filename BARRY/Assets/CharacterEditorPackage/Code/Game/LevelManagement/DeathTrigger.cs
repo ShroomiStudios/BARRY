6799a453a681bc314bcbb05889a30fc1c5a65e2b
shroomigames <108271494+ShroomiStudios@users.noreply.github.com>
2022-06-29 13:35:40 +0100

    a

---
+++ BARRY/Assets/CharacterEditorPackage/Code/Game/LevelManagement/DeathTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+//--------------------------------------------------------------------
+//When the player enters, respawn them
+//--------------------------------------------------------------------
+public class DeathTrigger : MonoBehaviour {
+
+    public Transform respawnpoint;
+
+    void OnTriggerEnter(Collider a_Collider)
+    {
+        ControlledCapsuleCollider controlledCapsuleCollider = a_Collider.GetComponent<ControlledCapsuleCollider>();
+        if (controlledCapsuleCollider != null)
+        {
+            //Prevent death state to be used if the collider is no-clipping
+            if (controlledCapsuleCollider.AreCollisionsActive())
+            { 
+                Debug.Log("Death triggered by: " + transform.name);
+                controlledCapsuleCollider.transform.position = respawnpoint.transform.position;
+            }
+        }
+    }
+}
